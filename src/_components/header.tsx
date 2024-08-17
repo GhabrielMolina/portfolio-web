67d290299d368c0a9ef79fa65eca904b2400ba46
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-08-17 15:31:24 -0300

    feat: add header component

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,9 +1,51 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/_components/ui/navigation-menu";
+import { SunMoon } from "lucide-react";
+
 export function Header() {
   return (
-    <div>
-      <div>
-        <h1 className="uppercase">Ghabriel<span>Molina</span></h1>
+    <div className="p-5 w-full">
+      <div className="bg-white rounded-full w-full flex items-center justify-between p-2">
+        {/* LOGO && NAME  */}
+        <div className="flex items-center gap-2 ">
+          <img
+            src="logo.png"
+            alt="GH Logo"
+            className="h-10 bg-black rounded-full"
+          />
+          <h1 className="uppercase  text-zinc-950">
+            Ghabriel<span className="font-bold">Molina</span>
+          </h1>
+        </div>
+
+        {/* LIGHT/DARK MODE && MENU INPUT*/}
+        <div className="flex items-center gap-3">
+          <div className="bg-black rounded-full p-2 cursor-pointer">
+            <SunMoon className="text-white text-sm" />
+          </div>
+
+          {/* TODO: Fazer menu de navegação */}
+          <NavigationMenu className="bg-black rounded-full  hover:bg-yellow-400">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="text-sm text-white">
+                  Menu
+                </NavigationMenuTrigger>
+
+                <NavigationMenuContent className=" bg-yellow-500">
+                  <NavigationMenuLink className="text-black font-semibold"></NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
       </div>
     </div>
-  )
+  );
 }
